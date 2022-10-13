9ec8f0c3705bd533457ac5f2e57d1e61c93f570b
Clara Chen <clara@lucidinside.com>
2022-10-13 18:59:06 -0500

    10/13/2022

--- src/pages/resume/components/Education.js
+++ src/pages/resume/components/Education.js
@@ -140,19 +140,6 @@ const Education = () => {
                     <h2 className={classnames('text-3xl', 'text-bold', 'pb-2')}>Rice University
                       <p className={classnames('text-xl', 'text-bold', 'pb-2')}>Master of Computer Science</p>
                     </h2>
-                    {/* <p className={classnames('text-sm', 'text-gray-700', 'pb-4')}>A platform that enables enterprises to build AI applications without having to know how to build AI</p>
-                    <div>
-                    • Improved user experience by supporting http live streaming (hls), user can autoplay previews (hls) when
-                    hover and dragging segments (hls) trained from AI model for e-commerce (React.js)
-                    </div>
-                    <div>
-                    • Earned totally over $1000/month income of three company contracts by building three mockups
-                    responsive websites and five MVP features on website in six months (React.js)
-                    </div>
-                    <div>
-                    • Reduced 100% time of manual user account creations by integrating 3 types auth flow in one week with
-                    Firebase - Password Authentication & Email Verification, Phone Number, Google Sign-in
-                    </div> */}
                 </div>
                 </FadeInRight>
             </div>
@@ -162,7 +149,6 @@ const Education = () => {
                     <div className={classnames('text-left-fade-in')}>
                         <div className={classnames('w-56')}>
                             <p>Sep. 2014 - Jul. 2017</p>
-                            {/* <p className={classnames('text-xl', 'text-bold', 'pb-2')}>M.B.A in Information Management</p> */}
                         </div>
                     </div>
                 </FadeInLeft>
@@ -179,26 +165,7 @@ const Education = () => {
                         >
                             <h2 className={classnames('text-3xl', 'text-bold', 'pb-4')}>National Central University
                               <p className={classnames('text-xl', 'text-bold', 'pb-2')}>M.B.A in Information Management</p>
-                            </h2>
-                            {/* <p className={classnames('text-1xl', 'text-bold', 'text-gray-800', 'pb-2')}>Baidu Map with Leaflet Layer (Frontend)</p>
-                            <div>
-                                • Refactored code to reduce 90 % maintenance time. Translated three types of coordinates WGS84, GCJ02,
-                                and BD09 from activity routes record of device to display on the Baidu map
-                            </div>
-                            <div>
-                                • Provided more than 1 million users to access their activity routes on the website
-                            </div>
-                            <p className={classnames('text-base', 'text-bold', 'text-gray-800', 'pt-4', 'pb-2')}>Super Set (Backend)</p>
-                            <div>
-                            • Provided a big data visualization platform for marketing to trace parallel goods by multiple dimensions
-                            </div>
-                            <div>
-                            • Migrated 14 Asia countries historical data over 700 million records only in 5 hours from MSSQL to MySQL
-                            </div>
-                            <div>
-                            • Reduced 100% time of manual migration time by building daily synchronization cron jobs to migrate daily
-                            updating data of users’ activity data from 14 Asia countries (Java, Spring Boot, MySQL)
-                            </div> */}
+                            </h2>                            
                         </div>
                     </div>
                 </FadeInRight>
@@ -227,15 +194,6 @@ const Education = () => {
                             <h2 className={classnames('text-3xl', 'text-bold', 'pb-2')}>National Dong Hwa University 
                               <p className={classnames('text-xl', 'text-bold', 'pb-2')}>B.A in Information Management</p>
                             </h2>
-                            {/* <p className={classnames('text-sm', 'text-gray-700', 'pb-4')}>
-                                A social travel platform that connects travelers with locals who share common interests and lifestyle
-                            </p>
-                            <div>
-                            • Built everything from scratch and earned 7000+ registered members from 45 countries and successfully matched more than 1500 members worldwide.
-                            </div>
-                            <div>
-                            • Designed the whole system and architecture of frontend, backend, and database to solve the business need (PHP, MySQL, Bootstrap.js, AWS)
-                            </div> */}
                         </div>
                     </div>
                 </FadeInRight>
